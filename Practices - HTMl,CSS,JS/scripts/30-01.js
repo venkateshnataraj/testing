de5fb9834345bc659c04ea7e304ea46e1f15e2c7
Venkatesh Nataraj <140409100+venkateshnataraj@users.noreply.github.com>
2024-03-10 01:30:18 +0530

    Added a new files

---
+++ Practices - HTMl,CSS,JS/scripts/30-01.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const newa = arr.map((item) => item * 2);
+
+// console.log(newa);
+// const a = 4;
+// const b = 4;
+
+// "use strict";
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let newarray;
+
+for (let i = 1; i <= array.length; i++) {}
